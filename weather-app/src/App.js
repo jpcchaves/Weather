fb60e057094fc796febc3f03b8d9c3d69266c3ff
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-29 20:11:05 -0300

    geolocation verification and sketch of the project

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Hook
+  const [location, setLocation] = useState(false);
+
+  // useEffect
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    });
+  }, []);
+
+  // checking if the user gave us permission to access this location
+  if (location == false) {
+    return <Fragment>Habilite a localização no browser.</Fragment>;
+  } else {
+    return (
+      <Fragment>
+        <h3>Clima na sua localidade</h3>
+        <hr />
+        <ul>
+          <li>Temperatura Atual: </li>
+          <li>Temp max</li>
+          <li>Temp min</li>
+          <li>Pressão</li>
+          <li>Umidade</li>
+        </ul>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
